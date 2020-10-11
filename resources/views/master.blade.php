2f7c987c52f52e2ecbf59567a4e95a6909632a57
Anton Baeckelandt <anton.baeckelandt@student.howest.be>
2020-10-11 18:34:13 +0200

    Small fixes

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -14,9 +14,9 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <ul class="collapse navbar-collapse">
-                <h1 class="navbar-brand">Anton Baeckelandt</h1>
+                <a href="{{ route('posts') }}"><h1 class="navbar-brand">Anton Baeckelandt</h1></a>
                 <div class="navbar-nav ml-auto">
-                    <li class="nav-item mr-2"><a class="nav-link" href="{{ route('blog') }}">Blog</a></li>
+                    <li class="nav-item mr-2"><a class="nav-link" href="{{ route('posts') }}">Blog</a></li>
                     <li class="nav-item mr-2"><a class="nav-link" href="#">CV</a></li>
                     <li class="nav-item mr-2"><a class="nav-link" href="{{ route('about') }}">Over mij...</a></li>
                 </div>
