b222f2b19c277263edc2f08c007787e9debcf1e8
Anton Baeckelandt <anton.baeckelandt@student.howest.be>
2020-10-11 17:30:42 +0200

    Add loading of posts

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,7 +2,15 @@
 
 @section("title", "Blog")
 
+@section('head')
+    @parent
+@endsection
+
 @section("main")
     <h1>Blog</h1>
-    <!-- TODO: Load posts from DB -->
+    @foreach($posts as $post)
+        <h2>{{ $post->title }}</h2>
+        <h3>Gepost op {{ (new Carbon\Carbon($post->created_at))->toDateString() }}</h3>
+        <p>{{ $post->content }}</p>
+    @endforeach
 @endsection
