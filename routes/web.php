29657d439b8b1934705badcdd6d9870f1b2a720f
Anton Baeckelandt <anton.baeckelandt@student.howest.be>
2020-10-11 17:04:55 +0200

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', "App\Http\Controllers\PostController@showPosts") -> name("posts");
+Route::get('/about', function(){
+    return view("about");
+}) -> name("about");
