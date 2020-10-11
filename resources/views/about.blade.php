8c35873f71da8e5bbaa7a27a7ead9b45dbbad696
Anton Baeckelandt <anton.baeckelandt@student.howest.be>
2020-10-11 18:34:04 +0200

    Add about me page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,14 @@
 @section("title", "Over mij")
 
 @section("main")
-    <h1>Over mij</h1>
-    <p>Hier komt tekst</p>
+    <div class="row">
+        <div class="col">
+            <img src="/image/antonbaeckelandt.jpg" alt="Foto Anton Baeckelandt" title="Anton Baeckelandt" id="profilepicture" class="img-thumbnail rounded float-right h-25" />
+            <h1 class="display-3 mb-4">Over mij</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hac habitasse platea dictumst vestibulum. Eget est lorem ipsum dolor sit amet consectetur adipiscing. Nam libero justo laoreet sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Quis eleifend quam adipiscing vitae proin sagittis nisl. Ultrices neque ornare aenean euismod elementum nisi. Malesuada proin libero nunc consequat interdum. Eu volutpat odio facilisis mauris sit. Elit sed vulputate mi sit amet mauris commodo quis. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Volutpat sed cras ornare arcu dui vivamus. Nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet. Metus aliquam eleifend mi in. Non quam lacus suspendisse faucibus interdum posuere lorem. Nisi porta lorem mollis aliquam ut porttitor. Fringilla phasellus faucibus scelerisque eleifend. Fermentum et sollicitudin ac orci phasellus egestas tellus. At varius vel pharetra vel turpis nunc eget lorem dolor.
+
+Ultrices eros in cursus turpis massa tincidunt dui ut ornare. Senectus et netus et malesuada fames. Eget dolor morbi non arcu risus quis varius quam quisque. Sit amet commodo nulla facilisi nullam. Ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus. Cras pulvinar mattis nunc sed blandit libero. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Mattis vulputate enim nulla aliquet porttitor lacus luctus. Non quam lacus suspendisse faucibus interdum posuere lorem.</p>
+     
+        </div>
+    </div>
 @endsection
