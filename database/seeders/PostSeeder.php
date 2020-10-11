af55a879a258cfa5ffa27d3baff5b13b05cc3114
Anton Baeckelandt <anton.baeckelandt@student.howest.be>
2020-10-11 16:25:42 +0200

    Add post seeder

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Mock posts
+        $count = 10;
+        for($i = 0; $i < $count; $i++){
+            DB::table('posts')->insert([
+                'title' => Str::random(10),
+                'content' => Str::random(500),
+            ]);
+        }
+    }
+}
