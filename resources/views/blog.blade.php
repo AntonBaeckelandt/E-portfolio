b33d406de9199693064ddee4a6d5953a7d7b45eb
Anton Baeckelandt <anton.baeckelandt@student.howest.be>
2020-10-17 17:02:58 +0200

    Update front css

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -8,21 +8,7 @@
 
 @section("main")
     <h1 class="display-3 text-center mb-4">Blog</h1>
-    @foreach($posts as $post)
-        <article class="card mb-4 flex-row flex-wrap pt-3 pb-3">
-            <img src="/image/posts/{{ $post->image }}" alt="{{ $post->title }}" title="{{ $post->title }}" class="mw-25 w-25 mx-3" />
-            <div class="card-body px-2 w-25 ml-4 d-flex flex-column">
-                <h2 class="card-title">{{ $post->title }}</h2>
-                <div>
-                    <i class="material-icons text-muted">calendar_today</i><h3 class="card-subtitle mb-2 text-muted d-inline ml-2 align-top">Gepost op {{ (new Carbon\Carbon($post->created_at))->toDateString() }}</h3>
-                </div>
-                <p class="card-text mw-25 pr-3">{{ $post->description }}</p>
-                <a href="{{ route('post', $post->id) }}" class="btn btn-primary ml-auto mt-auto">Lees meer</a>
-            </div>
-        </article>
-    @endforeach
-
-    <div class="row justify-content-between">
+    <div class="row justify-content-between mb-3">
         @foreach($posts as $post)
         <article class="card" style="width: 18rem;">
             <img class="card-img-top px-3" src="/image/posts/{{ $post->image }}" alt="{{ $post->title }}" title="{{ $post->title }}">
@@ -45,6 +31,7 @@
         </article>
         @endforeach
     </div>
-  
-    {{ $posts->render() }}
+    <div class="row pagination">
+        {{ $posts->render() }}
+    </div>
 @endsection
