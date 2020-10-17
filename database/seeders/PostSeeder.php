c908e1e9ce662a4415c80198626ce559c1c11814
Anton Baeckelandt <anton.baeckelandt@student.howest.be>
2020-10-17 16:12:59 +0200

    Update database

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
 
 class PostSeeder extends Seeder
 {
@@ -22,7 +23,21 @@ class PostSeeder extends Seeder
                 'title' => Str::random(10),
                 'description' => Str::random(40),
                 'content' => Str::random(500),
+                'event_name'=> Str::random(10),
+                'event_date'=> $this->getRandomTimestamp(),
+                'event_location' => Str::random(10),
             ]);
         }
     }
+
+    function getRandomTimestamp($backwardDays = null)
+	{
+		if ( is_null($backwardDays) )
+		{
+			$backwardDays = -800;
+		}
+		$backwardCreatedDays = rand($backwardDays, 0);
+		return Carbon::now()->addDays($backwardCreatedDays)->addMinutes(rand(0, 60 * 23))->addSeconds(rand(0, 60));
+	}
+
 }
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -21,5 +21,19 @@
             </div>
         </article>
     @endforeach
+
+    <article class="card" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </article>
     {{ $posts->render() }}
 @endsection
