d1566f5097529ee7c9c6ea892fce9fdbd899c6cc
anton.baeckelandt <anton.baeckelandt@student.howest.be>
2021-04-05 15:34:53 +0200

    Add footer

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,6 +9,7 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/bootstrap.min.css') }}" />
         <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/custom.css') }}" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
     @show
 </head>
 <body>
@@ -30,8 +31,26 @@
     <main class="container">
         @yield('main')
     </main>
-    <footer>
-        <!-- TODO -->
+    <footer class="bg-dark text-center text-white fixed-bottom">
+        <div class="container p-2 pt-4">
+            <section class="mb-2">
+                <a class="btn btn-outline-light btn-floating m-1"
+                   href="https://www.linkedin.com/in/anton-baeckelandt/"
+                   role="button"
+                   target="_blank">
+                    <i class="bi bi-linkedin"></i>
+                </a>
+                <a class="btn btn-outline-light btn-floating m-1"
+                   href="https://github.com/AntonBaeckelandt/"
+                   role="button"
+                   target="_blank">
+                    <i class="bi bi-github"></i>
+                </a>
+            </section>
+            <section>
+                <p> Site gemaakt door Anton Baeckelandt</p>
+            </section>
+        </div>
     </footer>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
