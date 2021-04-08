2cbfab160511cd1d4c88936007cb2c1e37aae1fe
anton.baeckelandt <anton.baeckelandt@student.howest.be>
2021-04-08 16:16:35 +0200

    Update design

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -11,23 +11,24 @@
     <div class="row justify-content-between mb-3">
         @foreach($posts as $post)
         <article class="card mb-3">
-            <div class="card-header d-flex flex-column justify-content-between">
-                <img class="card-img-top px-3 mb-3" src="/image/posts/{{ $post->image }}" alt="{{ $post->title }}" title="{{ $post->title }}">
-
-                @if (!empty($post->event_name))
-                <ul class="event-data px-0 mb-0">
-                    <li class="mb-2"><span class="material-icons">record_voice_over</span><span class="ml-2 align-top">{{ $post->event_name }}</span></li>
-                    <li class="mb-2"><span class="material-icons">today</span><span class="ml-2 align-top">{{ (new Carbon\Carbon($post->event_date))->toDateString() }}</span></li>
-                    <li class="mb-0"><span class="material-icons">place</span><span class="ml-2 align-top">{{ $post->event_location }}</span></li>
-                </ul>
-                @endif
-            </div>
-            <div class="card-body pt-2">
-                <h4 class="card-title h3">{{ $post->title }}</h4>
+            <div class="card-header">
+                <h4 class="card-title h3 mb-3">{{ $post->title }}</h4>
                 <div>
-                    <i class="material-icons text-muted">calendar_today</i><h5 class="h6 card-subtitle mb-2 text-muted d-inline ml-2 align-top">Gepost op {{ (new Carbon\Carbon($post->created_at))->toDateString() }}</h5>
+                    <i class="material-icons text-muted h6">calendar_today</i><h5 class="h6 card-subtitle mb-2 text-muted d-inline ml-2 align-top">Gepost op {{ (new Carbon\Carbon($post->created_at))->toDateString() }}</h5>
                 </div>
                 <p class="card-text">{{ $post->description }}</p>
+            </div>
+            <div class="card-body pt-2  d-flex flex-column justify-content-between">
+                <img class="card-img-top px-3 mb-3 mt-2" src="/image/posts/{{ $post->image }}" alt="{{ $post->title }}" title="{{ $post->title }}">
+
+                @if (!empty($post->event_name))
+                    <ul class="event-data px-0 mb-0">
+                        <li class="mb-2"><span class="material-icons">record_voice_over</span><span class="ml-2 align-top">{{ $post->event_name }}</span></li>
+                        <li class="mb-2"><span class="material-icons">today</span><span class="ml-2 align-top">{{ (new Carbon\Carbon($post->event_date))->toDateString() }}</span></li>
+                        <li class="mb-0"><span class="material-icons">place</span><span class="ml-2 align-top">{{ $post->event_location }}</span></li>
+                    </ul>
+                @endif
+
             </div>
             <a href="{{ route('post', $post->id) }}" class="btn btn-success mt-auto ml-auto mr-auto mb-2">Lees meer</a>
         </article>
